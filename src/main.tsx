d72ed51a8ea068aa590a0b50d17b03f7b9c3d151
Atavixion <jukiangm@gmail.com>
2026-07-10 22:02:37 +0200

    Begin migration of website

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { BrowserRouter, Routes, Route, Link } from 'react-router'
+
+import Index from './pages/index.tsx'
+import About from './pages/about.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+    {/* Nav */}
+    <nav>
+      <Link to='/'>Home</Link> |{' '}
+      <Link to='/about'>About</Link>
+    </nav>
+
+    {/* Routes */}
+    <Routes>
+      <Route path='/' element={<Index />} />
+      <Route path='/about' element={<About />} />
+    </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import './index.css'
+
+
+export default function Index() {
+    return <h1>Hello World!</h1>
+    //return <div dangerouslySetInnerHTML={{__html: oldHtml}}/>
+}
